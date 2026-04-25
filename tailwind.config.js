dc76866fa988352f1f1e2fb9fdd71ddcd6662fca
KhamidovKhusniddin <ki770760636@gmail.com>
2026-04-25 06:29:05 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: '#8B0000',
+          gold: '#D4AF37',
+          dark: '#1a1a1a',
+        }
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
